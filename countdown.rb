8b2bb32a0d88056715eb39e0451eb5755b27699a
Carrie Mathieu <carrieburks@gmail.com>
2020-08-08 20:23:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,4 +7,10 @@ def countdown(number)
   puts "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep
+countdown_with_sleep(number)
+while number != 0
+    puts "#{number} SECOND(S)!"
+    number = number - 1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
